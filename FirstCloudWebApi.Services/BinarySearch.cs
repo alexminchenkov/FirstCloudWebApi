53a1dcc9899984af5a4c2618fef2cfe76ae301ae
Aliaksei Minchankou <Aliaksei.Minchankou-contractor@nreca.coop>
2017-03-06 20:34:48 -0500

    Added BinarySearch

---
+++ FirstCloudWebApi.Services/BinarySearch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstCloudWebApi.Services
+{
+    public class BinarySearch
+    {
+        public bool Search(List<int> source, int searchingValue)
+        {
+            if (source.Count == 0)
+            {
+                return false;
+            }
+
+            if (source.Count == 1 && source[0] != searchingValue)
+            {
+                return false;
+            }
+
+            int half = source.Count/2;
+            if (source[half] == searchingValue)
+            {
+                return true;
+            }
+
+            if (source[half] >= searchingValue)
+            {
+                return this.Search(source.Take(half).ToList(), searchingValue);
+            }
+
+            return this.Search(source.Skip(half).ToList(), searchingValue);
+        }
+
+        public bool SearchWithIndexes(List<int> source, int searchingValue)
+        {
+            return this.Search(source, searchingValue, 0, source.Count - 1);
+        }
+
+        private bool Search(List<int> source, int searchingValue, int startIndex, int endIndex)
+        {
+            if (startIndex > endIndex)
+            {
+                return false;
+            }
+
+            var mediumIndex = (endIndex + startIndex)/2;
+            if (source[mediumIndex] == searchingValue)
+            {
+                return true;
+            }
+
+            if (source[mediumIndex] <= searchingValue)
+            {
+                return this.Search(source, searchingValue, mediumIndex + 1, endIndex);
+            }
+
+            return this.Search(source, searchingValue, 0, mediumIndex - 1);
+        }
+    }
+}
---
+++ FirstCloudWebApi.Tests/BinarySearchTests.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using FirstCloudWebApi.Services;
+using NUnit.Framework;
+
+namespace FirstCloudWebApi.Tests
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        private readonly BinarySearch classUnderTest = new BinarySearch();
+
+        [Test]
+        public void Search_GivenSearchingItemInFirstHalfOfSource_ShouldReturnTrue()
+        {
+            // Arrange
+            List<int> source = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int searchingValue = 2;
+
+            // Action
+            var result = this.classUnderTest.Search(source, searchingValue);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Search_GivenSearchingItemInSecondHalfOfSource_ShouldReturnTrue()
+        {
+            // Arrange
+            List<int> source = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int searchingValue = 8;
+
+            // Action
+            var result = this.classUnderTest.Search(source, searchingValue);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Search_GivenNoSearchingItemInSource_ShouldReturnFalse()
+        {
+            // Arrange
+            List<int> source = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int searchingValue = 18;
+
+            // Action
+            var result = this.classUnderTest.Search(source, searchingValue);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Search_GivenNoEmptySource_ShouldReturnFalse()
+        {
+            // Arrange
+            List<int> source = new List<int>();
+            int searchingValue = 18;
+
+            // Action
+            var result = this.classUnderTest.Search(source, searchingValue);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void SearchWithIndexes_GivenSearchingItemInFirstHalfOfSource_ShouldReturnTrue()
+        {
+            // Arrange
+            List<int> source = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int searchingValue = 2;
+
+            // Action
+            var result = this.classUnderTest.SearchWithIndexes(source, searchingValue);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SearchWithIndexes_GivenSearchingItemInSecondHalfOfSource_ShouldReturnTrue()
+        {
+            // Arrange
+            List<int> source = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int searchingValue = 9;
+
+            // Action
+            var result = this.classUnderTest.SearchWithIndexes(source, searchingValue);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SearchWithIndexes_GivenNoSearchingItemInSource_ShouldReturnFalse()
+        {
+            // Arrange
+            List<int> source = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int searchingValue = 18;
+
+            // Action
+            var result = this.classUnderTest.SearchWithIndexes(source, searchingValue);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void SearchWithIndexes_GivenNoEmptySource_ShouldReturnFalse()
+        {
+            // Arrange
+            List<int> source = new List<int>();
+            int searchingValue = 18;
+
+            // Action
+            var result = this.classUnderTest.SearchWithIndexes(source, searchingValue);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
